93042abb11528c0e986ab7f7822b14f3cfa4b266
Prathmesh Chopade <prathmeshchopade96@gmail.com>
2026-02-26 19:00:11 +0530

    Daily Java practice: swap two numbers

---
+++ daily-code/SwapNumbers_j253.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+        System.out.println("Before swap: a = " + a + " b = " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swap: a = " + a + " b = " + b);
+    }
+}

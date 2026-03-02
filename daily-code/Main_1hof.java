ae233f9130591d63635c876cf1d7e5332c7c5218
Prathmesh Chopade <prathmeshchopade96@gmail.com>
2026-03-02 19:00:10 +0530

    Daily Java practice: print even numbers 1 to 20

---
+++ daily-code/Main_1hof.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int num;
+        for (num = 1; num <= 20; num++) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
+    }
+}

cc302305d041519f1490d02c6ae39f48761473fe
Prathmesh Chopade <prathmeshchopade96@gmail.com>
2026-03-05 19:00:10 +0530

    Daily Java practice: print numbers 1 to 10

---
+++ daily-code/Main_9oc4.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int num;
+        for (num = 1; num <= 10; num++) {
+            System.out.println(num);
+        }
+    }
+}
